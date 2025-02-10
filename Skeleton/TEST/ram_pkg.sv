483a5af30b0c336a6a3e9fd7362b34191504e8d4
Harsh Navadiya <hnavadiya1802@gmail.com>
2025-02-10 11:09:45 +0530

    Add TOP and PKG file

---
+++ Skeleton/TEST/ram_pkg.sv
@@ -0,0 +1,25 @@
+`ifndef RAM_PKG_SV
+`define RAM_PKG_SV
+
+`include "ram_defines.sv"
+`include "ram_interface.sv"
+
+package ram_pkg;
+
+//  event item_done_ev;
+
+        
+        `include "ram_trans.sv"
+        `include "ram_generator.sv"
+        `include "ram_driver.sv"
+        `include "ram_monitor.sv"
+        `include "ram_ref_model.sv"
+        `include "ram_scoreboard.sv"
+        `include "ram_environment.sv"
+        `include "ram_base_test.sv"
+
+  //add all file till test, don't miss the order
+  
+endpackage
+
+`endif
---
+++ Skeleton/TOP/ram_tb_top.sv
@@ -0,0 +1,32 @@
+module ram_tb_top();
+
+  import ram_pkg::*;
+
+  bit clk;
+
+  initial begin
+    clk = 0;
+    forever #5 clk = ~clk;
+  end
+  
+  //take instance of actual interface
+
+  ram_interface rif(clk);
+  
+  
+  //take intance of test class
+  ram_base_test ram_test;
+  
+  
+  //instantiate design
+  ram DUT (.clk(clk),
+           .rst(inf.rst),
+		   .wr_enb(inf.we)
+  
+  //
+  initial
+    //create test
+	//call require test methods and finaly call $finish
+	:
+
+endmodule
