574cdba7a4eabe7d2724195e80ca8c478ab83a02
Harsh Navadiya <hnavadiya1802@gmail.com>
2025-02-17 17:28:24 +0530

    Misc

--- Skeleton/ENV/ram_trans.sv
+++ Skeleton/ENV/ram_trans.sv
@@ -5,7 +5,6 @@
 typedef enum bit [1:0] {IDLE, READ, WRITE, SIM_RW} trans_kind_e;
 
 class ram_trans;
-
     static int read_count;
     static int write_count;
 
@@ -18,11 +17,13 @@ class ram_trans;
     randc bit [`ADDR_WIDTH - 1 : 0] rd_addr;
     randc bit [`ADDR_WIDTH - 1 : 0] wr_addr;
     rand bit [`DATA_WIDTH - 1 : 0] wr_data;
+    bit [`DATA_WIDTH - 1 : 0] rd_data; // New non-randomized field for read data
 
     constraint addr {wr_addr <= 2**`ADDR_WIDTH ; rd_addr <= 2**`ADDR_WIDTH; };
 
     function void transaction();
-        $display("Transaction Type = %s, rd_addr = %b, wr_addr = %b, wr_data = %b", trans_kind, rd_addr, wr_addr, wr_data);
+        $display("Transaction Type = %s, rd_addr = %b, wr_addr = %b, wr_data = %b, rd_data = %b", 
+                 trans_kind, rd_addr, wr_addr, wr_data, rd_data);
     endfunction
 
     function void prop();
@@ -35,10 +36,10 @@ class ram_trans;
             WRITE   :     write_count = write_count + 1;
             SIM_RW  :     begin
 
-                                read_count = read_count + 1;
-                                write_count = write_count + 1;
+                            read_count = read_count + 1;
+                            write_count = write_count + 1;
 
-                          end
+                         end
         endcase
     endfunction
 
